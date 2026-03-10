df5cad8793ea060cccf2cd9a0dbe9c1cf5199be3
Sharhan Kaji <shabibbhaikaji@rrc.ca>
2026-03-10 14:22:02 -0500

    feat: fix import path for HTTP_STATUS in health check controller and add Loan interface model

--- src/api/v1/controllers/healthController.ts
+++ src/api/v1/controllers/healthController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { successResponse } from "../models/responseModel";
-import { HTTP_STATUS } from "../../constants/httpConstants";
+import { HTTP_STATUS } from "../../../constants/httpConstants";
 
 export const healthCheck = (req: Request, res: Response): void => {
   res.status(HTTP_STATUS.OK).json(successResponse({}, "Health check passed"));
