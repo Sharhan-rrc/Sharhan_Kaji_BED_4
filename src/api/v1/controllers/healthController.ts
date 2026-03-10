99b09f2150b9b5597beebc801e6abeb1d6aa5362
Sharhan Kaji <shabibbhaikaji@rrc.ca>
2026-03-10 14:20:42 -0500

    feat: add health check controller for application status monitoring

---
+++ src/api/v1/controllers/healthController.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from "express";
+import { successResponse } from "../models/responseModel";
+import { HTTP_STATUS } from "../../constants/httpConstants";
+
+export const healthCheck = (req: Request, res: Response): void => {
+  res.status(HTTP_STATUS.OK).json(successResponse({}, "Health check passed"));
+};
